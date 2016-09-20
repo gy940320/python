a5ae467a05f347ace2f2c072ea111a55c45694d0
gy940320 <18646211101@163.com>
2016-09-20 23:30:00 +0800

    format

--- urllib.py
+++ urllib.py
@@ -1,6 +1,6 @@
 import urllib2
 def html(url):
-	    html=urllib2.urlopen(url).read()
-	        return html
-	    url='http://www.baidu.com'
-	    print html(url)
+	html=urllib2.urlopen(url).read()
+	return html
+	url='http://www.baidu.com'
+	print html(url)
